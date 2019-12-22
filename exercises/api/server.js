3789c1092111726ce72f728a430137f0874e33a6
Bohdan Kovalenko <bohdan.kovalenko@chisw.com>
2019-12-22 22:54:55 +0200

    Fixed error with static server

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -2,18 +2,28 @@ const http = require('http')
 const url = require('url')
 const fs = require('fs')
 const path = require('path')
+var mime = require('mime-types')
+
 
 /**
  * this function is blocking, fix that
  * @param {String} name full file name of asset in asset folder
  */
-const findAsset = (name) => {
-  const assetPath = path.join(__dirname, 'assets', name)
-  return fs.readFileSync(assetPath, {encoding: 'utf-8'}).toString()
-}
 
 const hostname = '127.0.0.1'
 const port = 3000
+const index_file = 'index.html'
+const file_not_found = '404.html'
+
+const getMimetype = (file_name) => {
+  return {'Content-Type': mime.lookup(file_name)}
+}
+
+const returnFile = (res, file_name, status) => {
+  res.writeHead(status, getMimetype(file_name))
+  res.write(fs.readFileSync(file_name, {encoding: 'utf-8'}).toString())
+  res.end()
+}
 
 // log incoming request coming into the server. Helpful for debugging and tracking
 const logRequest = (method, route, status) => console.log(method, route, status)
@@ -21,16 +31,19 @@ const logRequest = (method, route, status) => console.log(method, route, status)
 const server = http.createServer((req, res) => {
   const method = req.method
   const route = url.parse(req.url).pathname
+  let file_name = route
   // this is sloppy, especially with more assets, create a "router"
   if (route === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(findAsset('index.html'))
+    file_name = index_file
+  }
+  let assetPath = path.join(__dirname, 'assets', file_name)
+  if (fs.existsSync(assetPath)) {
+    returnFile(res, assetPath, 200)
     logRequest(method, route, 200)
-    res.end()
-  } else {
-    // missing asset should not cause server crash
-    throw new Error('route not found')
-    res.end()
+  }
+  else {
+    returnFile(res, path.join(__dirname, 'assets', file_not_found), 404)
+    logRequest(method, route, 404)
   }
   // most important part, send down the asset
 })
